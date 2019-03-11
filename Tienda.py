94d2ad9d6163e5b796623b4441cd3134e9eb8837
Martharl <48331252+Martharl@users.noreply.github.com>
2019-03-11 10:24:06 -0600

    Tienda.py

---
+++ Tienda.py
@@ -0,0 +1,13 @@
+#Romero Martha
+
+#Definaos la clase como tienda
+class Tienda
+  
+  #Coloquemos dos argumentos
+  def __ init __ (self,n,productos):
+    self.nombre = n
+    self.producto = producto
+
+  #ahora demostremos los detalles de los argumentos
+  def imprimirdetalles(self):
+    self.producto,imprimirdetalles()
